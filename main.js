6507a253ba56f202a10d42948853106c6e62a2e3
Christine Lyston <calyston.14@gmail.com>
2021-04-01 11:32:33 +0100

    feat: sections display on scroll

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ function animateSlides() {
     const revealImg = slide.querySelector('.reveal-img');
     const img = slide.querySelector('img');
     const revealText = slide.querySelector('.reveal-text');
-    //GSAP
+
+    //GSAP Slider Timeline
     const slideTimeline = gsap.timeline({
       defaults: { duration: 2, ease: 'power2.inOut' }
     });
@@ -20,6 +21,21 @@ function animateSlides() {
     slideTimeline.fromTo(img, { opacity: '0' }, { opacity: '1' }, '-=2');
     slideTimeline.fromTo(revealText, { opacity: '1' }, { opacity: '0' }, '-=1');
     slideTimeline.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=3');
+
+    //Create Scene
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false
+    })
+      .setTween(slideTimeline)
+      // .addTo(controller)
+      .addIndicators({
+        colorStart: 'white',
+        colorTrigger: 'white',
+        name: 'slide'
+      })
+      .addTo(controller)
   });
 }
 

3dc4d1bc1fa98bba3a14eb50dfc5914b84b0614a
Christine Lyston <calyston.14@gmail.com>
2021-04-01 10:31:04 +0100

    feat: slides and nav animating using gsap

--- main.js
+++ main.js
@@ -11,5 +11,16 @@ function animateSlides() {
     const revealImg = slide.querySelector('.reveal-img');
     const img = slide.querySelector('img');
     const revealText = slide.querySelector('.reveal-text');
-  })
+    //GSAP
+    const slideTimeline = gsap.timeline({
+      defaults: { duration: 2, ease: 'power2.inOut' }
+    });
+
+    slideTimeline.fromTo(revealImg, { x: '0%' }, { x: '100%' });
+    slideTimeline.fromTo(img, { opacity: '0' }, { opacity: '1' }, '-=2');
+    slideTimeline.fromTo(revealText, { opacity: '1' }, { opacity: '0' }, '-=1');
+    slideTimeline.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=3');
+  });
 }
+
+animateSlides();

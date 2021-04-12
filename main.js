087537197f364e2406ef5b27894fc2fa1380ba8c
Christine <calyston.14@gmail.com>
2021-04-12 11:20:22 +0100

    feat: barba tags added to html

--- main.js
+++ main.js
@@ -99,6 +99,26 @@ function navToggle(e) {
 
 }
 
+//Barba Page Transitions
+barba.init({
+  views: [
+    {
+      namespace: 'home'
+      beforeEnter() {
+        animateSlides();
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        controller.destroy();
+      }
+    },
+    {
+      namespace: 'fashion'
+    }
+  ]
+})
+
 //Event Listeners
 hamburgerMenu.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
